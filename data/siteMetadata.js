ec93ef6cef2c5e0eda3607d76b31cf62ced6dc16
Naveen Kumar Matturthi <naveen3273@users.noreply.github.com>
2023-05-02 11:42:12 +0530

    Initial commit
    Site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Blog of Naveen Kumar M',
+  author: 'Naveen Kumar M',
+  headerTitle: 'nvn blog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
@@ -10,12 +10,12 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
+  email: 'naveenkumar3273@gmail.com',
+  github: 'https://github.com/naveen3273',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/naveenkumarmatturthi',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
